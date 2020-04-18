b1c813e3f5f55e6697858173acff795db61ba6bc
sami15 <36919637+sami15@users.noreply.github.com>
2020-04-18 19:27:45 +0530

    Add files via upload

---
+++ animation/scripts/script.js
@@ -0,0 +1,7 @@
+console.log('fuck')
+var myAnimation = new hoverEffect({
+	parent: document.querySelector('.images'),
+	image1: './images/img1.jpg',
+	image2: './images/img2.jpg',
+	displacementImage: 'images/fluid.jpg'
+});
